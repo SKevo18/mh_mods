201db13b4f44c346169302b16a0035995a1d2060
Kevin Svitač <kevin@svit.ac>
2024-02-09 20:42:46 +0000

    Modify repo URL

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -11,7 +11,7 @@ var rootCmd = &cobra.Command{
 	Use:   "mhmods <pack/unpack/packmod> <game ID> <data file location> <input/output folder path(s)>",
 	Short: "Moorhuhn modding tool for packing and unpacking game data files",
 	Long: `A Fast and Flexible Moorhuhn modding tool built with love in Go.
-	Complete documentation (including game IDs) is available at http://github.com/SKevo18/mhmods`,
+	Complete documentation (including game IDs) is available at http://github.com/SKevo18/mh_mods`,
 	Args: cobra.MinimumNArgs(4),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Use)
