18123e20f9f9f325abfc6910ad9562cb44ad6b2c
Kevin Svitač <kevin@svit.ac>
2024-02-11 15:48:18 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Builds the project. Requires Go 1.22
+
+cd "$(dirname "$0")"
+
+export GOOS=$(uname -s | tr '[:upper:]' '[:lower:]')
+export GOARCH=$(uname -m | sed 's/aarch64/arm64/' | sed 's/x86_64/amd64/')
+
+go build -o build/
