5b4c5c17df60bac912bc34d9e7559ffb6b4cbf46
Kevin Svitač <kevin@svit.ac>
2024-03-01 14:24:59 +0000

    working webserver (i think)

--- webserver/app.py
+++ webserver/app.py
@@ -1,34 +1,57 @@
-from quart import Quart, render_template, request, send_file
-from modder import GAMES
+from quart import Quart, abort, render_template, request, send_file
+from modder import GAMES, pack
 
 from tempfile import NamedTemporaryFile
 
 WEBSERVER = Quart(__name__)
 
 
+def _get_game(game_id: str):
+    game = GAMES.get(game_id)
+
+    if game is None:
+        abort(404, f"Game with ID `{game_id}` not found.")
+
+    return game
+
+
 @WEBSERVER.get("/")
+@WEBSERVER.get("/game")
 async def index():
     return await render_template("games.html", games=GAMES.values())
 
 
-@WEBSERVER.get("/<string:game_id>")
+@WEBSERVER.get("/game/<string:game_id>")
 async def game(game_id: str):
-    return await render_template("game.html", game=GAMES[game_id])
+    return await render_template("game.html", game=_get_game(game_id))
 
 
-@WEBSERVER.get("/<string:game_id>/packmod")
-async def packmod(game_id: str):
-    mods = request.args.getlist("mods")
-    game = GAMES[game_id]
+@WEBSERVER.get("/game/<string:game_id>/packmod")
+async def packmods(game_id: str):
+    mods = request.args.getlist("mod")
+    game = _get_game(game_id)
     mods_to_pack = [mod for mod in game.get_mods() if mod.id in mods]
 
     if not mods_to_pack:
-        return "No mods selected/invalid IDs.", 400
+        return await abort(400, "No valid mods selected to pack.")
 
-    with NamedTemporaryFile(suffix=game.original_datafile.suffix) as f:
-        await mods_to_pack[0].pack(f.name, mods_to_pack)
+    with NamedTemporaryFile(suffix=game.original_datafile.suffix, delete=False) as f:
+        out, err, code = await pack(game, f.name, mods_to_pack)
+ 
+        if err is not None:
+            err = err.decode("utf-8")
+            print("bin error:", err)
+        else:
+            err = "no stderr"
+
+        if code != 0:
+            return await abort(
+                500,
+                f"An error occurred while packing the mod (shell status code: {code}).\n\n{err}",
+            )
+
         return await send_file(
             f.name,
             as_attachment=True,
-            attachment_filename=f"{game.original_datafile.name}",
+            attachment_filename=f"{game.out_filename}",
         )
--- webserver/modder.py
+++ webserver/modder.py
@@ -16,7 +16,7 @@ DATAFILE_ROOT = Path(__file__).parent.parent / "data"
 MHMODS_BINARY = (
     Path(__file__).parent.parent
     / "build"
-    / ("mhmods" if os_name == "nt" else "mhmods.exe")
+    / ("mhmods.exe" if os_name == "nt" else "mhmods")
 )
 
 
@@ -26,17 +26,28 @@ class Game:
     name: str
     original_datafile: Path
 
-    _mods: list[Mod] = field(default_factory=list)
+    mods_folder: t.Optional[Path] = None
+    mods: list[Mod] = field(default_factory=list)
+    out_filename: t.Optional[str] = None
+
+    def __post_init__(self):
+        self.mods_folder = self.mods_folder or MODS_ROOT / self.id
+        self.out_filename = self.out_filename or self.original_datafile.name
+        self.get_mods()
 
     def get_mods(self) -> list[Mod]:
-        if not self._mods:
-            self._mods = [
+        mods_dir = self.mods_folder
+        if not mods_dir or not mods_dir.exists():
+            return []
+
+        if not self.mods:
+            self.mods = [
                 Mod(id=mod_path.stem, game=self, path=mod_path)
-                for mod_path in (MODS_ROOT / self.id).iterdir()
+                for mod_path in (mods_dir).iterdir()
                 if mod_path.is_dir()
             ]
 
-        return self._mods
+        return self.mods
 
 
 @dataclass
@@ -44,49 +55,68 @@ class Mod:
     id: str
     game: Game
     path: Path
-    _readme: t.Optional[str] = None
+    readme: t.Optional[str] = None
 
-    def readme(self) -> t.Optional[str]:
-        if not self._readme:
+    def __post_init__(self):
+        self.get_readme()
+
+    def get_readme(self) -> t.Optional[str]:
+        if not self.readme:
             try:
-                with open(MODS_ROOT / self.id / "README.md", "r") as f:
-                    self._readme = f.read()
+                with open(self.path / "README.md", "r") as f:
+                    self.readme = f.read()
             except FileNotFoundError:
                 pass
 
-        return self._readme
+        return self.readme
 
-    async def pack(
-        self, output_path: t.AnyStr | Path, mods: list[Mod]
-    ) -> tuple[str, str]:
-        process = await create_subprocess_shell(
-            # mhmods packmod <game ID> <original data file> <output modded data file> <mod paths>... [flags]
-            f""""{MHMODS_BINARY}" packmod {self.game.id} "{self.game.original_datafile}" "{output_path}" {' '.join(f'"{mod.path}"' for mod in mods)}"""
-        )
 
-        out, err = await process.communicate()
-        return out.decode("utf-8"), err.decode("utf-8")
+async def pack(
+    game: Game, output_path: t.AnyStr | Path, mods: list[Mod]
+) -> tuple[bytes, bytes, t.Optional[int]]:
+    process = await create_subprocess_shell(
+        # mhmods packmod <game ID> <original data file> <output modded data file> <mod paths>... [flags]
+        f""""{MHMODS_BINARY}" packmods {game.id.split('.', 2)[0]} "{game.original_datafile}" "{output_path}" {' '.join(f'"{mod.path / 'source'}"' for mod in mods)}""",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
+    out, err = await process.communicate()
+    code = process.returncode
+    return out, err, code
 
 
 GAMES = {
     "mhk_1": Game(
         id="mhk_1",
         name="Moorhuhn Kart: Extra (XXL)",
+        out_filename="mhke.dat",
         original_datafile=DATAFILE_ROOT / "mhk_1.dat",
     ),
-    "mhk_2": Game(
-        id="mhk_2",
-        name="Moorhuhn Kart 2",
-        original_datafile=DATAFILE_ROOT / "mhk_2.dat",
+    "mhk_2.en": Game(
+        id="mhk_2.en",
+        name="Moorhuhn Kart 2 (English)",
+        original_datafile=DATAFILE_ROOT / "mhk_2.en.dat",
+        out_filename="mhk2-00.dat",
+        mods_folder=MODS_ROOT / "mhk_2",
+    ),
+    "mhk_2.de": Game(
+        id="mhk_2.de",
+        name="Moorhuhn Kart 2 (German)",
+        original_datafile=DATAFILE_ROOT / "mhk_2.de.dat",
+        out_filename="mhk2-00.dat",
+        mods_folder=MODS_ROOT / "mhk_2",
     ),
     "mhk_3": Game(
         id="mhk_3",
         name="Moorhuhn Kart 3",
+        out_filename="data.sar",
         original_datafile=DATAFILE_ROOT / "mhk_3.sar",
     ),
     "mhk_4": Game(
         id="mhk_4",
         name="Moorhuhn Kart: Thunder",
+        out_filename="data.sar",
         original_datafile=DATAFILE_ROOT / "mhk_4.sar",
     ),
 }
