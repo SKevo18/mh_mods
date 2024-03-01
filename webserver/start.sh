f973551e909b0aa7cf30694667468519b112db8b
Kevin Svitač <kevin@svit.ac>
2024-03-01 12:57:09 +0000

    tweaks to webserver start.sh

--- webserver/start.sh
+++ webserver/start.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-handle_sigint() {
-  echo "Killing..."
-  kill $pid1 $pid2
-}
-trap handle_sigint SIGINT
-
-if [ ! -d .venv ]; then
-  python3 -m venv .venv
-  source .venv/bin/activate
-  pip install -r requirements.txt
+if [ ! -d ../.venv ]; then
+  python3 -m venv ../.venv
+  source ../.venv/bin/pip install -r requirements.txt
 fi
 
 export PYTHONPATH=$(pwd)
-../.venv/bin/hypercorn app:WEBSERVER --bind=unix:./mhmods_flask.sock -k uvloop -m 007 -w 4 --timeout=300 &
-pid1=$!
-
-
-wait $pid1 $pid2
+../.venv/bin/hypercorn app:WEBSERVER --bind=unix:server.sock -k uvloop -m 007 -w 2 &
