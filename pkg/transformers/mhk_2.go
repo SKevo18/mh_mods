eca2d7628548254f34c774ee064c39e8be494b20
Kevin Svitač <kevin@svit.ac>
2024-03-02 14:21:42 +0100

    alright, apparently this works lmao someone will definitely read these messages

--- pkg/transformers/mhk_2.go
+++ pkg/transformers/mhk_2.go
@@ -130,7 +130,6 @@ func unpackMhk2(dataFileLocation string, outputPath string) error {
 
 		// decrypt, if necessary
 		if filepath.Ext(fileName) == ".txt" {
-			log.Printf("Decrypting `%s`...", fileName)
 			decryptConfig(fileData)
 		}
 
@@ -196,34 +195,33 @@ func readHeader(header [0x40]byte) (string, uint32) {
 
 // Encrypts MHK2 `.txt` config files.
 func encryptConfig(data []byte) {
-	key := uint16(0x1234)
+	key := uint(0x1234)
 
 	for i := range data {
-		oddBits := data[i] & 0x55
-		evenBits := data[i] & 0xAA
-		evenBits >>= 1
-		oddBits <<= 1
-
-		data[i] = (oddBits ^ evenBits) ^ byte(key&0xFF)
-		key = key*3 + 2&0xffff
+		uVar2 := data[i] & 0x55
+		cVar1 := data[i] & 0xAA
+		cVar1 >>= 0x1
+		uVar2 <<= 0x1
+		data[i] = (uVar2 ^ cVar1) ^ byte(key&0xFF)
+		key = (key * 0x3) + (0x2 & 0xffff)
 	}
 }
 
 // Decrypts MHK2 `.txt` config files.
-func decryptConfig(data []byte) {
-	key := uint16(0x1234)
+func decryptConfig(data []byte) []byte {
+	var uVar3 uint16 = 0x1234
 
 	for i := range data {
-		temp := data[i] ^ byte(key&0xFF)
+		uVar2 := (uVar3 & 0xFF) ^ uint16(data[i])
+		cVar1 := uVar2 & 0xFF
+		uVar3 = ((uVar3 * 0x3) + 0x2) & 0xFFFF
 
-		evenBits := temp & 0xAA
-		oddBits := temp & 0x55
-		evenBits <<= 1
-		oddBits >>= 1
-
-		data[i] = evenBits | oddBits
-		key = key*3 + 2&0xffff
+		data[i] = byte(((uVar2 >> 0x1) ^ (cVar1 << 0x1)) & 0xFF)
+		data[i] &= 0x55
+		data[i] ^= byte((cVar1 << 0x1) & 0xFF)
 	}
+
+	return data
 }
 
 // Generic function to pack or unpack MHK2 data files.
