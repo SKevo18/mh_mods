70d7375c5adea9d2d8fa46974fe32f6ce94e4a0c
Kevin Svitač <kevin@svit.ac>
2024-07-16 17:59:39 +0200

    gui cleanup

--- cli/src/cmd/root.go
+++ cli/src/cmd/root.go
@@ -12,7 +12,6 @@ var rootCmd = &cobra.Command{
 A fast and flexible Moorhuhn modding tool built with the Go programming language.
 More information is available at http://github.com/SKevo18/mh_mods`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: interactive mode (https://github.com/charmbracelet/huh)
 		cmd.Help()
 	},
 }
--- gui/README.md
+++
@@ -1,3 +0,0 @@
-# mhmods GUI
-
-This GUI sub-package is not currently maintained in favor of the development of a simpler and easier to maintain interactive CLI.
